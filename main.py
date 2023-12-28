bd29eb2e24c88a21ff630c43fb403cb8ecda31e5
Alvin Kimata <66922806+AlvinKimata@users.noreply.github.com>
2023-12-28 17:45:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,33 @@
-#!/usr/bin/env python
-# coding: utf-8
+def plot_pdf(data):
+    plt.figure(figsize=(14, 8))
 
-# # Salary Analysis
-# This code analyzes salary data from a European country. The dataset consists of samples of annual salaries. We aim to explore the distribution and calculate the mean salary.
-#Contents
-# 1. **Data Loading:** Load and combine salary data from multiple CSV files.
-# 2. **Exploratory Data Analysis (EDA):** Explore the distribution of salaries through visualizations and summary statistics.
-# 3. **Mean Salary Calculation:** Calculate the mean annual salary based on the dataset.
+    # Plot the histogram.
+    plt.hist(data, bins=30, density=True, alpha=0.6, color='g', edgecolor='black', label = 'PDF')
 
-import numpy as np
-import pandas as pd
-from scipy.stats import norm
-from scipy.integrate import quad
-import matplotlib.pyplot as plt
-from numpy.random import normal
+    # Fit a normal distribution to the data
+    mu, std = norm.fit(data)
 
-def load_and_rename_csv(file_path):
-    df = pd.read_csv(file_path)
-    df.columns = ['salary']
-    return df
-
-# Define the file paths
-file_paths = ['data0-1.csv', 'data1-1.csv', 'data3-2.csv', 'data4-2.csv', 'data5-1.csv', 'data7-1.csv', 'data8-1.csv']
-
-# Load and rename DataFrames using the function
-df_list = [load_and_rename_csv(file_path) for file_path in file_paths]
-
-# Concatenate the DataFrames
-df_combined = pd.concat(df_list, ignore_index=True)
+    # Plot the PDF
+    xmin, xmax = plt.xlim()
+    x = np.linspace(xmin, xmax, 100)
+    p = norm.pdf(x, mu, std)
+    plt.plot(x, p, 'k', linewidth=2)
+    plt.text(120000, 0.00002, f'Mean Salary: {mu:.2f}\nStandard deviation: {std:.2f}', fontsize=10)
 
 
-def convert_df_to_array(df):
-    data = df.values.tolist()
-    data = np.array(data)
-    data = np.squeeze(data)
-    return data
-
-data = convert_df_to_array(df = df_combined)
+    plt.title('Probability Density Function of Salary Distribution')
+    plt.xlabel('Salary')
+    plt.ylabel('Probability Density')
 
 
-plt.figure(figsize=(14, 8))
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
-# Plot the histogram.
-plt.hist(data, bins=30, density=True, alpha=0.6, color='g', edgecolor='black', label = 'PDF')
+    return mu, std
 
-# Fit a normal distribution to the data
-mu, std = norm.fit(data)
-
-# Plot the PDF
-xmin, xmax = plt.xlim()
-x = np.linspace(xmin, xmax, 100)
-p = norm.pdf(x, mu, std)
-plt.plot(x, p, 'k', linewidth=2)
-plt.text(150000, 0.00002, f'Mean Salary: {mu:.2f}', fontsize=10)
-    # plt.text(120000, 3200, f'Mean Salary: {sample_mean:.2f}\n mean probabilities: {np.mean(probabilities)}', fontsize=10)
-
-
-plt.title('Probability Density Function of Salary Distribution')
-plt.xlabel('Salary')
-plt.ylabel('Probability Density')
-
-
-plt.legend()
-plt.grid(True)
-plt.show()
-
+X = convert_df_to_array(df=df_combined)
+mean, std_dev = plot_pdf(data=X)
 
 # Display the result
-print(f"The mean annual salary is: {round(mu, 2)}")
+print(f"The mean annual salary is: {round(mean, 2)}")
